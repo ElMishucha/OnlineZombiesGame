19a8e9d7944f532c43b6d1f42f3e5e95791876ab
El Mishucha <mishutka.bolshakov@gmail.com>
2020-07-06 20:12:53 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    public float speed;
+    private void FixedUpdate()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        Vector2 move;
+        move.x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        move.y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(move);
+    }
+}
