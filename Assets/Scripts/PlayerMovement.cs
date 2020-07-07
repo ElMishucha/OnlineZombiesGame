e59b2371c63c83de3e843c0f6e5d1b99f4a4e5b5
El Mishucha <mishutka.bolshakov@gmail.com>
2020-07-07 08:50:19 +0300

    Added bullets and player rotate.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,5 +16,14 @@ public class PlayerMovement : NetworkBehaviour
         move.x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         move.y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         transform.Translate(move);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0f, 0f, -200f * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0f, 0f, 200f * Time.deltaTime);
+        }
     }
 }
